372fb83df43213d12ce2fdeaa822210b107bf255
Edwin Lee <leeed2001@gmail.com>
2018-11-15 12:19:47 -0600

    Create my own spritesheet, include a font file just in case

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <math.h>
+#include <sstream>
 
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
@@ -12,7 +14,9 @@ int main()
     sf::RenderWindow window(sf::VideoMode(c.initialWidth, c.initialHeight), "SFML works!");
     window.setTitle("Lee Game");
 
-    unsigned int const squareSpriteSideLength = 300;
+    unsigned int const fullSpriteSquareSideLength = 16*4;
+    unsigned int const spriteSquareBorderThickness = 1*4;
+    float animateIntervalInSeconds = 0.5f;
 
     sf::CircleShape shape1(100.f);
     shape1.setFillColor(sf::Color::Green);
@@ -22,13 +26,27 @@ int main()
     sf::Sound sound;
     sound.setBuffer(buffer);
 
-    sf::Event event{};
-    sf::Texture texture;
-    texture.loadFromFile("media/textures/dragonSheet.png");
-    sf::IntRect rectSourceSprite(0, 0, squareSpriteSideLength, squareSpriteSideLength);
-    sf::Sprite sprite(texture, rectSourceSprite);
+    sf::Texture texture2;
+    texture2.loadFromFile("media/textures/red_guy/SpriteSheet.png");
+    sf::IntRect redGuyRectangle(
+            spriteSquareBorderThickness,
+            spriteSquareBorderThickness,
+            fullSpriteSquareSideLength - 2*spriteSquareBorderThickness,
+            fullSpriteSquareSideLength - 2*spriteSquareBorderThickness
+    );
+    sf::Sprite redGuySprite(texture2, redGuyRectangle);
+    redGuySprite.setPosition(c.initialWidth / 2.0f, c.initialHeight / 2.0f);
 
+//    sf::Font font;
+//    font.loadFromFile("media/fonts/8bit_wonder/8BITWONDER.TTF");
+//    sf::Text text;
+//    text.setFont(font);
+//    std::stringstream ss;
+
+    sf::Event event{};
     sf::Clock clock;
+
+    bool redGuyGoingUp = true;
 
     while (window.isOpen()) {
         // while there are pending events...
@@ -60,39 +78,69 @@ int main()
         }
 
         // adjust which row of the sprite sheet we are on
+        auto const redGuyPosition = redGuySprite.getPosition();
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
         {
-            rectSourceSprite.top = squareSpriteSideLength*0;
+            redGuyRectangle.top = 0*fullSpriteSquareSideLength + spriteSquareBorderThickness;
+            redGuySprite.move(0, 1);
+            if ((redGuyPosition.y + fullSpriteSquareSideLength) > window.getSize().y) {
+                redGuySprite.setPosition(redGuyPosition.x, window.getSize().y - fullSpriteSquareSideLength);
+            }
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
         {
-            rectSourceSprite.top = squareSpriteSideLength*1;
+            redGuyRectangle.top = 1*fullSpriteSquareSideLength + spriteSquareBorderThickness;
+            redGuySprite.move(-1, 0);
+            if (redGuyPosition.x <= 0) {
+                redGuySprite.setPosition(0, redGuyPosition.y);
+            }
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
         {
-            rectSourceSprite.top = squareSpriteSideLength*2;
+            redGuyRectangle.top = 2*fullSpriteSquareSideLength + spriteSquareBorderThickness;
+            redGuySprite.move(1, 0);
+            if ((redGuyPosition.x + fullSpriteSquareSideLength) > window.getSize().x) {
+                redGuySprite.setPosition(window.getSize().x - fullSpriteSquareSideLength, redGuyPosition.y);
+            }
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
         {
-            rectSourceSprite.top = squareSpriteSideLength*3;
+            redGuyRectangle.top = 3*fullSpriteSquareSideLength + spriteSquareBorderThickness;
+            redGuySprite.move(0, -1);
+            if (redGuyPosition.y <= 0) {
+                redGuySprite.setPosition(redGuyPosition.x, 0);
+            }
         }
 
         // adjust which column we are on based on a timer - one column per second
-        if (clock.getElapsedTime().asSeconds() > 0.5f) {
-            if (rectSourceSprite.left >= squareSpriteSideLength * 2)
-                rectSourceSprite.left = 0;
-            else
-                rectSourceSprite.left += squareSpriteSideLength;
+        if (clock.getElapsedTime().asSeconds() > animateIntervalInSeconds) {
+            if (redGuyRectangle.left == spriteSquareBorderThickness) {
+                redGuyRectangle.left = 1*fullSpriteSquareSideLength + spriteSquareBorderThickness;
+                redGuyGoingUp = true;
+            } else if (redGuyRectangle.left == 1*fullSpriteSquareSideLength + spriteSquareBorderThickness) {
+                if (redGuyGoingUp) {
+                    redGuyRectangle.left = 2*fullSpriteSquareSideLength + spriteSquareBorderThickness;
+                } else {
+                    redGuyRectangle.left = spriteSquareBorderThickness;
+                }
+            } else if (redGuyRectangle.left == 2*fullSpriteSquareSideLength + spriteSquareBorderThickness) {
+                redGuyRectangle.left = 1*fullSpriteSquareSideLength + spriteSquareBorderThickness;
+                redGuyGoingUp = false;
+            } else {
+                // shouldn't be able to get here
+            }
+
             clock.restart();
         }
 
         // then draw the appropriate region of the sprite sheet
-        sprite.setTextureRect(rectSourceSprite);
+        redGuySprite.setTextureRect(redGuyRectangle);
 
         window.clear();
-        //window.draw(shape1);
-        window.draw(sprite);
+        window.draw(redGuySprite);
+        // window.draw(text);
         window.display();
+
     }
 
     return 0;
